4272698ce47391494f872a2e918787b54da5bb49
Cailleux <acailleux@lerebours.org>
2018-09-20 16:46:27 +0200

    Fin de l'exercice 1

--- PHP/getLesServices.php
+++ PHP/getLesServices.php
@@ -2,7 +2,7 @@
 include 'cnx.php';
 $sql = $cnx->prepare("select idService, nomService from service where idCateg = ".$_GET['id']);
 $sql->execute();
-echo "<select id=''lstServices' >";
+echo "<select id='lstServices' >";
 foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
 {
     echo "<option value='".$ligne['idService']."'>".$ligne['nomService']."</option>";
