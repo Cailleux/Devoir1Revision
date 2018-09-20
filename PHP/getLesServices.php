e42b4cf8b93978869655646eae442181fc7d6643
Cailleux <acailleux@lerebours.org>
2018-09-20 16:48:29 +0200

    Modification de la sortie des services

--- PHP/getLesServices.php
+++ PHP/getLesServices.php
@@ -2,12 +2,12 @@
 include 'cnx.php';
 $sql = $cnx->prepare("select idService, nomService from service where idCateg = ".$_GET['id']);
 $sql->execute();
-echo "<select id='lstServices' >";
+echo "<p id='lstServices' >";
 foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
 {
     echo "<option value='".$ligne['idService']."'>".$ligne['nomService']."</option>";
 }
 
-echo "</select>";
+echo "</p>";
 
 ?>
